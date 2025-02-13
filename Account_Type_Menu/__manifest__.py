304f4481488d2ddfddcee2083419594129f88bb9
alihassanmostafa <alihassan.mostafa@gmail.com>
2025-02-13 17:59:53 +0200

    show account type menu

---
+++ Account_Type_Menu/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Account Type Menu',
+     "summary":"Custom Account Type",
+    'depends': ['account'],
+     'author':'Petra Software',
+    'company': 'Petra Software',
+    'maintainer': 'Petra Software',
+    'website':'www.t-petra.com',
+     'license': 'LGPL-3',
+    'data': [
+        'views/account_type_menu.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    #  'price':10,
+    # 'currency':'USD'
+   
+}
